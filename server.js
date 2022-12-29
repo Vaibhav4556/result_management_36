5bd68f01905d89a016f92cfbd05cba542a30e43d
Vaibhav4556 <burdev741@gmail.com>
2022-12-29 13:27:27 +0530

    render

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ app.use("/api/results/", resultsRoute);
 
 const port = process.env.PORT || 4000;
 
+// render deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 

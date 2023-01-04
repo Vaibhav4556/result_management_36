03d0af5aff72bcd34fc9a1e75fca688332e01231
Vaibhav4556 <burdevaibhav786@gmail.com>
2023-01-04 14:00:28 +0530

    4jan commit

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use("/api/results/", resultsRoute);
 const port = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
